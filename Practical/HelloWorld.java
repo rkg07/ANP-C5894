8ac8250fd0a392fe2db033d161a02ff76a785c68
rkg07 <137861824+rkg07@users.noreply.github.com>
2023-07-03 11:45:28 +0530

    Add files via upload

--- Practical/HelloWorld.java
+++ Practical/HelloWorld.java
@@ -1,8 +1,11 @@
-//to print simple "Hello World"
-class HelloWorld
-{
-public static void main(String[] args)
-{
-System.out.println("Hello World");
-}
+package Oops;
+
+//Program to prient hello world
+class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+	
 }
