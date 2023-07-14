1c494fb961c71973bbf050b550242fa665899e61
rkg07 <137861824+rkg07@users.noreply.github.com>
2023-07-14 13:57:38 +0530

    Add files via upload

---
+++ Practical/DoWhileLoopEx.java
@@ -0,0 +1,21 @@
+package Loops;
+
+//Simple do-while loop example
+public class DoWhileLoopEx
+{
+
+	public static void main(String[] args)
+	{
+		//Declaring and initializing i to 1
+		int i=0;
+		do
+		{
+			//print hello world
+			System.out.println("Hello World");
+			i++;
+		}
+		//Test expression
+		while(i<6);
+
+	}
+}
