b5ac4ed4e34004e4ecf9ae6389172766c9ec1185
rkg07 <137861824+rkg07@users.noreply.github.com>
2023-07-14 13:58:50 +0530

    Add files via upload

---
+++ Practical/PrintNumbers.java
@@ -0,0 +1,18 @@
+package Loops;
+
+//print numbers from 1 to 10
+public class PrintNumbers
+{
+	public static void main(String[] args) 
+	{
+		//num initialized to 1
+      int num = 1;
+      
+      //condition to check num is less than equal to 10
+      while (num <= 10)
+      {
+          System.out.println("Number is:"+num);
+          num++;
+      }
+  }
+}
