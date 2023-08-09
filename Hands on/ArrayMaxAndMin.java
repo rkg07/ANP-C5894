94ba629c03fb51db35d56f958a82ab41480b73da
rkg07 <137861824+rkg07@users.noreply.github.com>
2023-08-10 01:28:31 +0530

    Add files via upload

---
+++ Hands on/ArrayMaxAndMin.java
@@ -0,0 +1,55 @@
+package Array;
+
+/* 
+ Given an array of integers, write a Java program to find 
+ the maximum and minimum elements in the array.
+ */
+
+import java.util.Scanner;
+public class ArrayMaxAndMin
+{
+	public static void main(String[] args) 
+	{
+		//Scanner class object
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the size of the array: ");
+		int size = sc.nextInt();
+		
+		int[]num = new int [size];
+		
+		System.out.println("Enter " + size + "integers: ");
+		int i;
+		
+		// Initializing and Declaring the value
+		for (i=0; i<size; i++)
+		{
+			num[i] = sc.nextInt();
+			
+		}
+		
+		// Displaying the element
+		int min = num[0];
+		int max = num[0];
+		
+		for (i=0; i<size; i++)
+		{
+			if(num[i]>max)
+			{
+				max = num[i];
+			}
+			
+			if(num[i]< min)
+			{
+				min = num[i];
+			}
+		}
+		
+		System.out.println("Maximun Number: " + max);
+		System.out.println("Minimum Number: " + min);
+		
+		sc.close();
+
+	}
+
+}
